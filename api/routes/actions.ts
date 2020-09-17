286be7f428d6d287ada9b50d2f771a0b923557cc
klazowski <klazowski+gitub@gmail.com>
2020-09-17 19:25:33 +0200

    Added actions route

---
+++ api/routes/actions.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const router: express.Router = express.Router();
+/*
+  - wystawic 1 REST endpoint, który przyjmuje POSTem:
+  {userId: ..., gameId: ...., action: ...}
+*/
+
+export default router;
