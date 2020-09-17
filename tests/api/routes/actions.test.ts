4c1449a425a76c3b3dee90d30bef5543909bf528
klazowski <klazowski+gitub@gmail.com>
2020-09-17 20:01:37 +0200

    Added action routes tests

--- tests/api/routes/actions.test.ts
+++ tests/api/routes/actions.test.ts
@@ -10,16 +10,53 @@ chai.use(chaiHttp);
   {userId: ..., gameId: ...., action: ...}
 */
 
-/*
-expect(response).to.have.status(201);
-
+describe('Actions routes', function () {
+  describe('"/actions" GET request', function () {
+    it('"/" GET request should return json with list of actions', function (done: Mocha.Done) {
+      chai
+        .request(actions)
+        .get('/')
+        .end(function (error, response) {
+          expect(error).to.be.null;
+          expect(response).to.have.status(200);
+          expect(response).to.be.json;
+          expect(response.body).to.have.key('actions');
+          expect(response.body.actions).to.be.an('array');
+          expect(response.body).to.have.key('_links');
+          expect(response.body._links).to.have.key('self');
+          done();
+        });
+    });
+  });
+  describe('"/actions" POST request', function () {
+    it('"/" POST request should add action to DB and return json with saved action', function (done: Mocha.Done) {
+      chai
+        .request(actions)
+        .post('/')
+        .send({ userId: 1, gameId: 1, action: 'skip' })
+        .end(function (error, response) {
+          expect(error).to.be.null;
+          expect(response).to.have.status(201);
+          expect(response).to.be.json;
           expect(response.body).to.have.key('action');
-          expect(response.body.action).to.have.key('id');
-          expect(response.body.action).to.have.key('link');
-*/
-
+          expect(response.body).to.have.key('_links');
+          expect(response.body._links).to.have.key('self');
+          expect(response.body._links).to.have.key('actions');
+          const savedAction = response.body;
+          chai
+            .request(actions)
+            .get('/')
+            .end(function (error, response) {
+              expect(error).to.be.null;
+              expect(response).to.have.status(200);
+              expect(response.body.actions).to.be.an('array').that.includes(savedAction);
+              done();
+            });
+        });
+    });
+  });
+});
 /*
-
 {
   action: {
     id: 102,
@@ -36,6 +73,4 @@ expect(response).to.have.status(201);
     },
   }
 }
-
-
 */
