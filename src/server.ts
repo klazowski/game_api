7938a24290d359f6982c66a5b5d2010b84683bbc
klazowski <klazowski+gitub@gmail.com>
2020-09-18 19:40:34 +0200

    Added GET '/' returning available enpoints

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import { Server } from 'http';
 import config from './config';
 
 const app: express.Application = express();
 
 const PORT = config.PORT;
+
+app.use(express.json());
+
+const endpoints = {
+  _links: {
+    self: {
+      href: '/',
+      methods: ['GET'],
+    },
+    actions: {
+      href: '/actions',
+      methods: ['GET', 'POST'],
+    },
+  },
+};
+
+app.get('/', (request: express.Request, response: express.Response): void => {
+  response.status(200).json(endpoints);
+});
 
 app.listen(PORT, (): void => {
   console.log(`Server listening on port ${PORT}`);
