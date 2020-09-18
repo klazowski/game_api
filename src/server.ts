ed0cfb241d57f628be0a7a5943b8dc4aa21eee09
klazowski <klazowski+gitub@gmail.com>
2020-09-18 19:05:04 +0200

    Added config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import config from './config';
 
 const app: express.Application = express();
 
-const PORT = 5001;
+const PORT = config.PORT;
 
 app.listen(PORT, (): void => {
   console.log(`Server listening on port ${PORT}`);
