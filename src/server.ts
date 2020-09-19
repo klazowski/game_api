526e61f4f8fc3953ef19145e370206920113ba35
klazowski <klazowski+gitub@gmail.com>
2020-09-19 10:24:36 +0200

    Added actions route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
 import config from './config';
 import endpoints from './services/endpoints';
+import actionsRoute from './api/routes/actions';
 
 const app: express.Application = express();
 
 const PORT = config.PORT;
 
 app.use(express.json());
+
+app.use('/actions', actionsRoute);
 
 app.get('/', (request: Request, response: Response): void => {
   response.status(200).json(endpoints);
