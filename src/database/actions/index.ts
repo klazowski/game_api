e4eedc9bb768436b54480b4c6284301f24fc3f4f
klazowski <klazowski+gitub@gmail.com>
2020-09-20 13:09:03 +0200

    Refactoring

--- src/database/actions/index.ts
+++ src/database/actions/index.ts
@@ -1,14 +1,5 @@
 import config from '../../config';
-import IDatabaseActions from '../interfaces/IDatabaseActions';
 import actionsMock from './actionsMock';
 import actionsMySql from './actionsMySql';
 
-let actions: IDatabaseActions;
-
-if (config.mockDb === 'true') {
-  actions = actionsMock;
-} else {
-  actions = actionsMySql;
-}
-
-export default actions;
+export default config.mockDb === 'false' ? actionsMySql : actionsMock;
