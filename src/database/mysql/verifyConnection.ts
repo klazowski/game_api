165883b247c00c20487eed2a4a389aefdfd9771f
klazowski <klazowski+gitub@gmail.com>
2020-09-20 12:21:58 +0200

    Remove log.

--- src/database/mysql/verifyConnection.ts
+++ src/database/mysql/verifyConnection.ts
@@ -4,11 +4,11 @@ const verifyConnection = async () => {
   return await connection
     .authenticate()
     .then((error) => {
-      console.log('Connection has been established successfully.');
+      //console.log('Connection has been established successfully.');
       return true;
     })
     .catch((error) => {
-      console.log('Unable to connect to the database:', error);
+      //console.log('Unable to connect to the database:', error);
       return false;
     });
 };
