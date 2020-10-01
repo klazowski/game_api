724057600c3e415974ee03136bad9c1cf2941555
klazowski <klazowski+gitub@gmail.com>
2020-10-01 17:46:07 +0200

    Hardcoded path for test

--- client/src/components/actions_list/actions.ts
+++ client/src/components/actions_list/actions.ts
@@ -18,10 +18,10 @@ interface IActionREST {
 }
 
 const getActions = async (): Promise<Action[]> => {
-  const result = await getData('/actions');
+  const result = await getData('http://localhost:5001/actions');
   if (!result.success) return []; // TODO: Proper error handling
 
-  const packagedActions: IActionREST[] = result.actions;
+  const packagedActions: IActionREST[] = result.actions.actions;
 
   const actions = packagedActions.map((action) => {
     return { id: action.id, userId: action.userId, gameId: action.gameId, action: action.action };
--- client/src/components/form/submit.ts
+++ client/src/components/form/submit.ts
@@ -1,6 +1,6 @@
 import { postData } from '../../connectors';
 const submit = async (newAction: NewAction): Promise<boolean> => {
-  const response = await postData('/actions', newAction);
+  const response = await postData('http://localhost:5001/actions', newAction);
   return response.success;
 };
 
