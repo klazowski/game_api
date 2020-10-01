c44cb909ea7bdde7d94ffa23e3a3c9d3237047c6
klazowski <klazowski+gitub@gmail.com>
2020-10-01 19:39:58 +0200

    Fixed mistake

--- client/src/connectors/index.ts
+++ client/src/connectors/index.ts
@@ -7,7 +7,7 @@ const getData = async (url: string = ''): Promise<any> => {
 const postData = async (url: string = '', data: {} = {}): Promise<{ success: boolean }> => {
   const response = await fetch(url, {
     method: 'POST',
-    headers: { 'content-type': 'application-json' },
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify(data),
   });
 
