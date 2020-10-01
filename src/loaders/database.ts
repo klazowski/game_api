4bb02ca30127d91e81fa87f34f7a85522c7c02d2
klazowski <klazowski+gitub@gmail.com>
2020-10-01 20:15:17 +0200

    Added population with mockup data when action table is empty

--- src/loaders/database.ts
+++ src/loaders/database.ts
@@ -1,6 +1,7 @@
 import { verifyConnection } from '../database/mysql/verifyConnection';
 import Action from '../models/Action';
 import waitPort from 'wait-port';
+import mockupActions from '../database/mockup';
 
 const setupDB = async (): Promise<boolean> => {
   await waitPort({ host: process.env.MYSQL_HOST, port: 3306 });
@@ -8,6 +9,12 @@ const setupDB = async (): Promise<boolean> => {
   if (!verifyConnection()) return false;
 
   await Action.sync();
+
+  if ((await Action.count()) === 0) {
+    console.log('No data found in action table. Populating with mockup data.');
+    Action.bulkCreate(mockupActions);
+    console.log('Database is now ready to use');
+  }
 
   console.log(`DB setup successful`);
 
